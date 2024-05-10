05886a2cac56f1ff54d724266d1caf4c25dfa9f1
bigoldcat123 <95476981+bigoldcat123@users.noreply.github.com>
2024-05-10 12:22:26 +0000

    e

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 
 import typescript from 'rollup-plugin-typescript2';
 import {defineConfig} from 'rollup'
+import { createFilter, dataToEsm } from '@rollup/pluginutils';
 
 export default defineConfig({
 	input:'src/index.ts',
@@ -8,5 +9,47 @@ export default defineConfig({
 		format:'cjs',
 		dir:'dist'
 	},
-	plugins:[typescript()]
+	plugins:[typescript(),json()]
 })
+
+function json(options = {}) {
+	const filter = createFilter(options.include, options.exclude);
+	const indent = 'indent' in options ? options.indent : '\t';
+  
+	return {
+	  name: 'json',
+  
+	  // eslint-disable-next-line no-shadow
+	  transform(code, id) {
+		// console.log(code);
+		console.log(id);
+		if (id.slice(-5) !== '.json' || !filter(id)) return null;
+		console.log('------------------')
+		try {
+		  const parsed = JSON.parse(code);
+		  console.log(dataToEsm(parsed, {
+			preferConst: options.preferConst,
+			compact: options.compact,
+			namedExports: options.namedExports,
+			includeArbitraryNames: options.includeArbitraryNames,
+			indent
+		  }),)
+		  console.log('------------------')
+		  return {
+			code: dataToEsm(parsed, {
+			  preferConst: options.preferConst,
+			  compact: options.compact,
+			  namedExports: options.namedExports,
+			  includeArbitraryNames: options.includeArbitraryNames,
+			  indent
+			}),
+			map: { mappings: '' }
+		  };
+		} catch (err) {
+		  const message = 'Could not parse JSON file';
+		  this.error({ message, id, cause: err });
+		  return null;
+		}
+	  }
+	};
+  }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import add from "./math/index";
-
+// import j from '../package.json'
 const realAdd = () => {
     console.log(add(1,2));
+    // console.log(j)
 }
 
 export default realAdd 
