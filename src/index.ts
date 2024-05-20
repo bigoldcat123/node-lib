fbe83ca2c8f2c4a54d3d6548b97a97aec843ea2b
bigoldcat123 <a1456870251@163.com>
2024-05-20 23:47:19 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import add from "./math/index";
 import j from '../package.json'
 import ja from '../package.json'
 import {name,version} from '../package.json'
+import p from 'node:child_process'
 const realAdd = () => {
     console.log(add(1,2));
     console.log(j);
-    console.log(name,version);
-    
+    console.log(name,version);   
 }
-realAdd()
+
+
 export default realAdd 
