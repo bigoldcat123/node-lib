0ed1cef8918658d54aac42149b275f8e221fb566
bigoldcat123 <a1456870251@163.com>
2024-07-20 14:24:01 +0800

    6

--- playground/test.js
+++ playground/test.js
@@ -1,4 +1,10 @@
 // const x= require('czh-node-liv')
 // console.log(x);
-import x from 'czh-node-liv'
-console.log(x.whoAmiI().age);
+import http from 'node:http'
+
+http.createServer((request,response) => {
+    request.on('data',data => {
+        console.log(data.toString());
+    })
+    response.end('hello world\n')
+}).listen(3000)
